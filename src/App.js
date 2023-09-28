f77c5b2cb1d653e80011a3c2bb465ebebed416c8
mtmwouter <74245592+mtmwouter@users.noreply.github.com>
2023-09-28 10:35:16 +0200

    Moved /public folder outside /my-app

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ function App() {
   return (<div className="App">
     <h1>Weather App</h1>
     <footer> 
-  This project was coded by Micky Wouter and is {" "}  
-  
-   <a href='https://github.com/mtmwouter/react-weather-app'
-  target='_blank'> open-sourced on Github.</a>
-  </footer>
-  </div>);
-
-
+      This project was coded by Micky Wouter and is {" "}  
+     <a href='https://github.com/mtmwouter/react-weather-app' target='_blank'> 
+        open-sourced on Github.
+      </a>
+    </footer>
+  </div>
+);
 }
 
 export default App;
